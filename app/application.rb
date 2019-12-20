508678f63600b6c5cdf64c8f3314b68c2e0811f1
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-20 19:12:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       @@items.each do |item|
-        resp.write "#{item}\n"
+        resp.write "#{item}  #{price}\n"
       end
 
     if req.path=="/items"
