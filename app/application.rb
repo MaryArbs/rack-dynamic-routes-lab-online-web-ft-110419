d0589b84839f3fe5d51186391a81a060413590bf
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-20 19:20:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,22 @@
 class Application
 
-  @@item = []
+  @@items = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}  #{price}\n"
-      end
-    end
+      item_name = req.path.split("/items/").last
 
-    if req.path=="/items"
-      resp.write "You requested this item."
+      item = @@items.find{|i| i.name == item_name}
+
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
     else
       resp.write "Route not found"
       resp.status = 404
